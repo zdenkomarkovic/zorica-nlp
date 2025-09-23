bf7f33c7067f1aa3e20e50a73d082e112795bf55
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-09-23 12:44:44 +0200

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://logoterapeut.rs', // Zameni svojim domenom
+    generateRobotsTxt: true, 
+    sitemapSize: 5000, 
+  };
